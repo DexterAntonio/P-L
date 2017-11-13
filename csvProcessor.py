61e16058f9df9989a341a960c4b14da1e559d5e6
Dexter Antonio <dexter.d.antonio@gmail.com>
2017-11-13 12:46:52 -0800

    changed it so that it is more object oriented in style

--- csvProcessor.py
+++ csvProcessor.py
@@ -8,7 +8,8 @@ import datetime
 import csv 
 import re 
 class Trade:
-    def __init__(self):
+    def __init__(self,row):
+        #defaults for testing purposes 
         self.dateTime = datetime.datetime.now() 
         self.quanity = 1.0 
         self.isBuy = False 
@@ -19,11 +20,54 @@ class Trade:
         self.netAmount = 80.0 #note that during initiation make sells automatically negative for quanity
         self.avgATR = 10.0
         self.symbol = "" 
+        
+        self.pRow(row)
+    def getNumber(self,numStr):
+        p = re.compile("[\d.]*")
+        par = re.compile("\(")
+        mult = 1
+        if par.match(numStr):
+            mult = -1 #checks to see if number is negative 
+    
+        for numStr in p.findall(numStr):
+            if not numStr == "":
+                return float(numStr)*mult   
+        
+    def pRow(self,row): #process a row of data 
+    #data format 
+    #[    0                    1         2                          3       4        5         6              7        8          9 ]
+    #['Trade Date',          'Type', 'Market Symbol',          'Buy/Sell', 'QTY', 'Price', 'Gross Amount', 'Comm', 'Reg Fee', 'Net Amount']
+    #['08/24/2017 1110ET', 'Option', 'SPXW  20170825P 2415.000', 'S',        '8', '$0.50 ', '$400.00 ', '($1.20)', '($0.33)', '$398.47 ']
+        #process date 
+        tmpStr = row[0]
+        tmpStr = tmpStr.split("E")[0] #removes ET part of the string 
+        self.dateTime = datetime.datetime.strptime(tmpStr,"%m/%d/%Y %H%M") #TO DO, SET TO EASTERN TIMEZONE 
+        #process option type
+        self.tradeType = row[1]
+        #process market symbol
+        tmpStrArray = row[2].split(" ")
+        self.symbol = tmpStrArray[0]
+        #if option tmpStrArray[1] and [2] have values DO LATER
+
+        #process QTY 
+        #turns it into a number
+        self.quanity = float(row[4].replace(",", ""))
+        if(row[3]=="S"): #if it is being sold short make quanity negative
+            self.quanity = self.quanity*(-1.0)
+        if(self.tradeType=="Option"): #the option quantities are in groups of 100 which is confusing 
+            self.quanity*=100 
+        self.price = self.getNumber(row[5]) 
+        self.gross = self.getNumber(row[6]) 
+        self.commision = self.getNumber(row[7])
+        self.regFee = self.getNumber(row[8]) 
+        self.netAmount = self.getNumber(row[9])
+            
 class FullTrade: 
     def __init__(self):
         self.symbol = ""
         self.tradeType = ""
         self.longOrShort = "long" 
+        self.grossProfit = 0.0 
         self.netProfit = 0.0
         self.netQuanity = 0.0
         self.duration = datetime.timedelta(hours=1)
@@ -36,6 +80,7 @@ class FullTrade:
         self.symbol = "" 
         self.tradeType = "" 
         self.longOrShort = "" 
+        self.grossProfit = 0.0
         self.netProfit = 0.0
         self.netQuanity = 0.0
         self.duration = datetime.timedelta(hours=1)
@@ -51,7 +96,8 @@ class FullTrade:
         #declare variables 
         for trades in self.listOfTrades:
             self.symbol = trades.symbol
-            self.tradeType = trades.tradeType 
+            self.tradeType = trades.tradeType
+            self.grossProfit += trades.price*trades.quanity 
             self.netProfit += trades.price*trades.quanity + trades.commision+trades.regFee
             self.netQuanity += trades.quanity 
             self.netComission += trades.commision
@@ -64,65 +110,12 @@ class FullTrade:
         self.listOfTrades.append(trade)
         self.updateAtributes()
     def getList(self):
-        return [self.symbol, self.tradeType, self.longOrShort, self.netProfit, self.netQuanity, self.duration, self.netComission, self.startDate, self.endDate,self.tradeNumber]
+        return [self.symbol, self.tradeType, self.longOrShort, self.grossProfit, self.netProfit, self.netQuanity, self.duration, self.netComission, self.startDate, self.endDate,self.tradeNumber]
     def getListNames(self):
-        return ["symbol","trade type","long/short", "netProfit","netQuanity","duration","netComission","start date","end date","trade number"]
-#for testing purposes 
-#==============================================================================
-# tmpTrade = Trade()
-# tmpFullTrade = FullTrade() 
-# print tmpFullTrade.netQuanity 
-# tmpFullTrade.addTrade(tmpTrade)
-# tmpFullTrade.updateAtributes()
-# print tmpFullTrade.startDate 
-# print tmpFullTrade.startDate   
-#==============================================================================
-#helper functions
-def getNumber(numStr):
-    p = re.compile("[\d.]*")
-    par = re.compile("\(")
-    mult = 1
-    if par.match(numStr):
-        mult = -1 #checks to see if number is negative 
+        return ["symbol","trade type","long/short", "gross Profit", "netProfit","netQuanity","duration","netComission","start date","end date","trade number"]
 
-    for numStr in p.findall(numStr):
-        if not numStr == "":
-            return float(numStr)*mult 
-        
-def pRow(row): #process a row of data 
-#data format 
-#[    0                    1         2                          3       4        5         6              7        8          9 ]
-#['Trade Date',          'Type', 'Market Symbol',          'Buy/Sell', 'QTY', 'Price', 'Gross Amount', 'Comm', 'Reg Fee', 'Net Amount']
-#['08/24/2017 1110ET', 'Option', 'SPXW  20170825P 2415.000', 'S',        '8', '$0.50 ', '$400.00 ', '($1.20)', '($0.33)', '$398.47 ']
-    #process date 
-    trade = Trade()
-    tmpStr = row[0]
-    tmpStr = tmpStr.split("E")[0] #removes ET part of the string 
-    trade.dateTime = datetime.datetime.strptime(tmpStr,"%m/%d/%Y %H%M") #TO DO, SET TO EASTERN TIMEZONE 
-    #process option type
-    trade.tradeType = row[1]
-    #process market symbol
-    tmpStrArray = row[2].split(" ")
-    trade.symbol = tmpStrArray[0]
-    #if option tmpStrArray[1] and [2] have values DO LATER
-
-    
-    
-    #process QTY 
-    #turns it into a number
-    trade.quanity = float(row[4].replace(",", ""))
-    if(row[3]=="S"): #if it is being sold short make quanity negative
-        trade.quanity = trade.quanity*(-1.0)
-    if(trade.tradeType=="Option"): #the option quantities are in groups of 100 which is confusing 
-        trade.quanity*=100 
-    trade.price = getNumber(row[5]) 
-    trade.gross = getNumber(row[6]) 
-    trade.commision = getNumber(row[7])
-    trade.regFee = getNumber(row[8]) 
-    trade.netAmount = getNumber(row[9])
-    return trade 
+#main function 
    
-    
 csvFileName = "ExecutionDetail.csv" 
 j = 0 
 inTrades = {} #incomplete trades
@@ -133,7 +126,7 @@ with open(csvFileName,'rb') as f:
         j += 1 
         
         if(j>2 and j<748): #need to find way to skip last line b/c that breaks things 
-            newTrade = pRow(row) 
+            newTrade = Trade(row) 
             if(newTrade.symbol in inTrades):
                 tmpFullTrade = inTrades[newTrade.symbol]
                 tmpFullTrade.addTrade(newTrade)
